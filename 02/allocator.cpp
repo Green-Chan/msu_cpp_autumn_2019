43080677fd3d4b10dbf3386997cfc7451855733b
Green-Chan <55838532+Green-Chan@users.noreply.github.com>
2019-10-23 14:59:15 +0300

    Update allocator.cpp

--- 02/allocator.cpp
+++ 02/allocator.cpp
@@ -1,29 +1,25 @@
 #include <cstdlib>
+#include "allocator.h"
 
-class LinearAllocator
-{
-public:
-    LinearAllocator(size_t maxSize) {
-        buf = static_cast<char *> (std::malloc(maxSize));
-        cur_ptr = buf;
-        max_size = maxSize;
+LinearAllocator::LinearAllocator(size_t maxSize) {
+    buf = static_cast<char *> (std::malloc(maxSize));
+    cur_ptr = buf;
+    max_size = maxSize;
+}
+
+char *LinearAllocator::alloc(size_t size) {
+    if (cur_ptr + size > buf + max_size) {
+        return nullptr;
     }
-    char* alloc(size_t size) {
-        if (cur_ptr + size > buf + max_size) {
-            return nullptr;
-        }
-        char *old_ptr = cur_ptr;
-        cur_ptr += size;
-        return old_ptr;
-    }
-    void reset() {
-        cur_ptr = buf;
-    }
-    ~LinearAllocator() {
-        std::free(buf);
-    }
-private:
-    char* buf;
-    char* cur_ptr;
-    size_t max_size;
-};
+    char *old_ptr = cur_ptr;
+    cur_ptr += size;
+    return old_ptr;
+}
+
+void LinearAllocator::reset() {
+    cur_ptr = buf;
+}
+
+LinearAllocator::~LinearAllocator() {
+    std::free(buf);
+}
