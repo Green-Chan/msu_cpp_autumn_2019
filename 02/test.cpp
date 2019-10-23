771524105deaef8bde0d6d52cbc1f07526b3c7be
Green-Chan <55838532+Green-Chan@users.noreply.github.com>
2019-10-23 17:36:10 +0300

    Update test.cpp

--- 02/test.cpp
+++ 02/test.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
 #include "allocator.h"
+#include <cstdlib>
 
 int main() {
-    char *p;
-    {
+    char *p, *q;
+    bool no_errors = 1;
     LinearAllocator all = LinearAllocator(5);
-    p = all.alloc(3);
-    std::cout << (int) p << std::endl;
-    std::cout << (int) all.alloc(3) << std::endl;
-    all.reset();
-    std::cout << (int) all.alloc(3) << std::endl;
-    std::cout << (int) all.alloc(2) << std::endl;
-    all.reset();
+    if (!(p = all.alloc(3)) || all.alloc(3)) {
+        no_errors = 0;
+        std::cout << "error on test 1" << std::endl;
     }
-    std::cout << *p;
+    all.reset();
+    if (!(q = all.alloc(3)) || !all.alloc(2)) {
+        no_errors = 0;
+        std::cout << "error on test 2" << std::endl;
+    }
+    if (q != p) {
+        no_errors = 0;
+        std::cout << "error on test 3" << std::endl;
+    }
+    all = LinearAllocator(100);
+    for (int i = 0; i < 100; i++) {
+        if (!(all.alloc(1))) {
+            no_errors = 0;
+            std::cout << "error on test 4" << std::endl;
+        }
+    }
+    if (all.alloc(1)) {
+        no_errors = 0;
+        std::cout << "error on test 5" << std::endl;
+    }
+    if (no_errors) {
+        std::cout << "no errors" << std::endl;
+    }
 }
