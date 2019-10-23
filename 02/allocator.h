7f293bf7dec0ee8b8ebeeb6916dd40c12ec1196c
Green-Chan <55838532+Green-Chan@users.noreply.github.com>
2019-10-23 14:59:46 +0300

    Update allocator.h

--- 02/allocator.h
+++ 02/allocator.h
@@ -6,4 +6,9 @@ public:
     LinearAllocator(size_t maxSize);
     char* alloc(size_t size);
     void reset();
+    ~LinearAllocator();
+private:
+    char* buf;
+    char* cur_ptr;
+    size_t max_size;
 };
